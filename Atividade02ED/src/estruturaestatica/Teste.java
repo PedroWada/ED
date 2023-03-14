448ad870f6faff783da41323e6b0f175ce47d83c
PEDRO WADA ARAUJO <pedro.waraujo@senacsp.edu.br>
2023-03-14 20:07:06 -0300

    Nem sei oq eu fiz

--- Atividade02ED/src/estruturaestatica/Teste.java
+++ Atividade02ED/src/estruturaestatica/Teste.java
@@ -3,30 +3,30 @@ package estruturaestatica;
 import javax.swing.JOptionPane;
 
 public class Teste {
-    
     public static void main(String[] args){
-        Fila<Integer> fila = new Fila<Integer>();
+        Fila<Integer> filaNormal = new Fila<Integer>();
+        Fila<Integer> filaPreferecial = new Fila<Integer>();
         int p = 0;
         int senha = 0;
         while(p != 999){
             p = Integer.parseInt(JOptionPane.showInputDialog(
             "Digite 1 para enfileirar \n"+
-            "Digite 2 para ver a fila \n"+
+            "Digite 2 para ver a fila \n"+1+
             "Digite 3 para desenfileirar \n"+
             "Digite 999 para finalizar"));
 
         switch(p){
             case 1:
                 senha+=1;
-                fila.enfileira(senha);
-                System.out.println(fila.toString());
+                filaNormal.enfileira(senha);
+                System.out.println(filaNormal.toString());
                 break;
             case 2:
-                System.out.println(fila.toString());
+                System.out.println(filaNormal.toString());
                 break;
             case 3:
-                fila.desenfileira();
-                System.out.println(fila.toString());
+                filaNormal.desenfileira();
+                System.out.println(filaNormal.toString());
                 break;
             case 999:
                 break;
